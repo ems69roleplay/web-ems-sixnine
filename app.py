36f196c0b2ea1ddfeeb3c472e9f55163e8ebd072
ems69roleplay <heroakun26@gmail.com>
2026-04-07 03:48:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ if 'logged_in' not in st.session_state:
 # --- 2. KONEKSI GOOGLE (SHEETS & DRIVE) ---
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 try:
-    creds = ServiceAccountCredentials.from_json_keyfile_name("kunci_sehat.json", scope)
+    creds_dict = st.secrets["my_google_creds"]
     client = gspread.authorize(creds)
     ID_SHEET = "1e28VoHNGJVVnEsSBVA7Plt03C9gaBSo6gRE6QYKRKko"
     
